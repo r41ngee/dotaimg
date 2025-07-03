e6e38e2c20d9b776126c472e01cb34336d235fa3
r41ngee <r41ngee@yandex.ru>
2025-07-04 01:12:04 +0300

    Добавлена основная логика в main.py для создания мозаики из изображений, а также реализована функция размещения изображений по координатам в файле placer.py с сортировкой по оттенку.

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 # В ФОТО РАСПОЛОЖИТЬ 34 * 41 = 1394 СКИЛЛА
+import selector
+import hue
+import placer
+from PIL import Image
+
 def main():
-    pass
+    rows, cols = 41, 17
+    pic_size = 128
+
+    pics = selector.recursive_search('dota_images')
+    print(len(pics))
+    pics_dict = {pic: hue.single_pic(pic) for pic in pics}
+    matrix = placer.place_in_coords(pics_dict, (cols, rows))
+    print(len(matrix))
+    print(len(matrix[0]))
+    
+    big_image = Image.new("RGB", (cols * pic_size, rows * pic_size))
+    for row in range(rows):
+        for col in range(cols):
+            with Image.open(matrix[row][col]) as img:
+                # Вставляем маленькое изображение в нужную позицию
+                big_image.paste(img, (col * pic_size, row * pic_size))
+
+    big_image.save('mosaic.png')
+    big_image.show()
 
 if __name__ == '__main__':
     main()
--- placer.py
+++ placer.py
@@ -1,2 +1,45 @@
-def place_in_coords(pics: dict[str, tuple[float, float, float]]):
-    ...
+def place_in_coords(
+        pics: dict[str, tuple[float, float, float]],
+        grid: tuple[int, int]
+    ):
+
+    pics = _sort_by_hue(pics)
+    # pics - отсортированный список кортежей (путь, (r, g, b))
+    # grid - (cols, rows)
+    cols, rows = grid
+    matrix = []
+    pics_list = [pic[0] for pic in pics]  # только пути к изображениям
+    total = cols * rows
+    # Если картинок меньше, чем нужно, дополним None
+    if len(pics_list) < total:
+        pics_list += [None] * (total - len(pics_list))
+    # Если больше, обрежем
+    pics_list = pics_list[:total]
+    for r in range(rows):
+        row = []
+        for c in range(cols):
+            row.append(pics_list[r * cols + c])
+        matrix.append(row)
+    return matrix
+    
+        
+def _sort_by_hue(pics: dict[str, tuple[float, float, float]]):
+    return sorted(pics.items(), key=_get_hue)
+
+def _get_hue(pic: tuple[str, tuple[float, float, float]]) -> float:
+    rgb = pic[1]
+    max_rgb = max(rgb)
+    min_rgb = min(rgb)
+    delta = max_rgb - min_rgb
+
+    if max_rgb == min_rgb:
+        return 0
+    
+    if max_rgb == rgb[0]:
+        return 60 * (((rgb[1] - rgb[2]) / delta ) % 6)
+    if max_rgb == rgb[1]:
+        return 60 * (((rgb[2] - rgb[0]) / delta) + 2)
+    if max_rgb == rgb[2]:
+        return 60 * (((rgb[0] - rgb[1]) / delta) + 4)
+    
+    
