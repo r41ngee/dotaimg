f054f262f128dfac98dfc6e7ac3613d3db442f56
r41ngee <r41ngee@yandex.ru>
2025-07-04 01:34:26 +0300

    Добавлена функция grid_div для подбора оптимальных размеров сетки изображений в main.py. Исправлен расчет количества строк и столбцов в зависимости от уникальных изображений. Обновлен вывод при некорректном размере сетки.

--- main.py
+++ main.py
@@ -4,12 +4,36 @@ import hue
 import placer
 from PIL import Image
 
+
+def grid_div(mul: int) -> tuple[int, int]:
+    """
+    Функция подбирает целочисленные размеры сетки (rows, cols) для заданного количества элементов mul,
+    при этом разница между количеством строк и столбцов минимальна.
+    Возвращает кортеж (rows, cols).
+    """
+    best_pair = (1, mul)
+    min_diff = abs(mul - 1)
+    for i in range(1, int(mul ** 0.5) + 1):
+        if mul % i == 0:
+            j = mul // i
+            if abs(i - j) < min_diff:
+                best_pair = (i, j)
+                min_diff = abs(i - j)
+    return best_pair
+
 def main():
-    rows, cols = 41, 17
+    rows, cols = 14, 88
     pic_size = 128
 
     pics = selector.recursive_search('dota_images')
-    print(len(pics))
+
+    pics_len = len(set(pics))
+    if pics_len != rows * cols:
+        print("Incorrect grid size!")
+        print("trying to guess the best grid size ...")
+        rows, cols = grid_div(pics_len)
+
+
     pics_dict = {pic: hue.single_pic(pic) for pic in pics}
     matrix = placer.place_in_coords(pics_dict, (cols, rows))
     print(len(matrix))
